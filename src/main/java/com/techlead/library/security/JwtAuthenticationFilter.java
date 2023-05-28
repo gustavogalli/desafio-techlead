a90865573ff0cd74677072cca0fe308d8c73947b
gustavogalli <gustavosgalli@gmail.com>
2023-05-27 22:53:39 -0300

    JwtAuthenticationFilter created

---
+++ src/main/java/com/techlead/library/security/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.techlead.library.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.techlead.library.domain.dtos.CredentialsDTO;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+    private JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        super();
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            CredentialsDTO credentialsDTO = new ObjectMapper().readValue(request.getInputStream(), CredentialsDTO.class);
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(credentialsDTO.getEmail(),credentialsDTO.getPassword(),new ArrayList<>());
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            return  authentication;
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String userName =((UserSS) authResult.getPrincipal()).getUsername();
+        String token = jwtUtil.generateToken(userName);
+        response.setHeader("access-control-expose-headers","Authorization");
+        response.setHeader("Authorization","Bearer " + token);
+    }
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        response.setStatus(401);
+        response.setContentType("application/json");
+        response.getWriter().append(json());
+    }
+
+    private CharSequence json() {
+        long date = new Date().getTime();
+        return "{"
+                + "\"timestamp\": " + date + ", "
+                + "\"status\": 401, "
+                + "\"error\": \"Unauthorized\", "
+                + "\"message\": \"Invalid email or password\", "
+                + "\"path\": \"/login\"}";
+    }
+}
+
