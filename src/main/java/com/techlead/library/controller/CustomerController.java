a984d08fa47f6d4967fd94c386faa6134c2d5a2e
gustavogalli <gustavosgalli@gmail.com>
2023-05-27 22:51:50 -0300

    CustomerController created

---
+++ src/main/java/com/techlead/library/controller/CustomerController.java
@@ -0,0 +1,63 @@
+package com.techlead.library.controller;
+
+import com.techlead.library.domain.dtos.CustomerDTO;
+import com.techlead.library.service.CustomerService;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    public static final String ID = "/{id}";
+
+    private CustomerService service;
+    private ModelMapper mapper;
+
+    public CustomerController(CustomerService service, ModelMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<CustomerDTO>> findAll(){
+        return ResponseEntity.ok().body(this.service.findAll()
+                .stream().map(book -> mapper.map(book, CustomerDTO.class)).collect(Collectors.toList()));
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @GetMapping(ID)
+    public ResponseEntity<CustomerDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), CustomerDTO.class));
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @PostMapping
+    public ResponseEntity<CustomerDTO> create(@Valid @RequestBody CustomerDTO dto) {
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri().path(ID).buildAndExpand(service.create(dto).getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @PutMapping
+    public ResponseEntity<CustomerDTO> update(@PathVariable Integer id, @Valid @RequestBody CustomerDTO dto){
+        dto.setId(id);
+        return ResponseEntity.ok().body(mapper.map(service.update(dto), CustomerDTO.class));
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @DeleteMapping(ID)
+    public ResponseEntity<CustomerDTO> delete(@PathVariable Integer id){
+        this.service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
