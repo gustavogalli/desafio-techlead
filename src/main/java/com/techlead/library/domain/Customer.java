86298b965efd7461fd33578adbb87696d583f296
gustavogalli <gustavosgalli@gmail.com>
2023-05-27 22:51:38 -0300

    Customer updated

--- src/main/java/com/techlead/library/domain/Customer.java
+++ src/main/java/com/techlead/library/domain/Customer.java
@@ -5,15 +5,26 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @Entity(name = "l02_customer")
 public class Customer extends Person {
+
+    public Customer(){
+        super();
+    }
+
+    public Customer(Integer id, String name, String cpf, String email, String password, List<Book> uploadedBooks, List<Book> borrowedBooks) {
+        super(id, name, cpf, email, password);
+        this.uploadedBooks = uploadedBooks;
+        this.borrowedBooks = borrowedBooks;
+    }
 
     @JsonIgnore
     @OneToMany(mappedBy = "uploader")
