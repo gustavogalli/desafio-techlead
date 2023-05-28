c679b6ee18ffb59d1f2ba9beff3b453fdbc8a4d8
gustavogalli <gustavosgalli@gmail.com>
2023-05-27 22:53:47 -0300

    JwtAuthorizationFilter created

---
+++ src/main/java/com/techlead/library/security/JwtAuthorizationFilter.java
@@ -0,0 +1,48 @@
+package com.techlead.library.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.io.IOException;
+
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+
+    private UserDetailsService userDetailsService;
+    private JwtUtil jwtUtil;
+
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, UserDetailsService  userDetailsService, JwtUtil jwtUtil) {
+        super(authenticationManager);
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            UsernamePasswordAuthenticationToken auth = getAuthentication(header.substring(7));
+            if(auth != null) {
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(String token) {
+        if(jwtUtil.tokenValido(token)){
+            String username = jwtUtil.getUsername(token);
+            UserDetails details = userDetailsService.loadUserByUsername(username);
+            return new UsernamePasswordAuthenticationToken(details.getUsername(), null, details.getAuthorities());
+        }
+        return null;
+    }
+}
